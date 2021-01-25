4aa5ab8439ff6400f928b6d03f3c6783b387cc98
Tsu-Hao Yang <des75421@gmail.com>
2021-01-25 21:40:07 +0800

    update

---
+++ mp3_player/src/store.js
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    active: 0,
+  },
+  mutations: {
+    setActive(state, payload) {
+      state.active = payload;
+    },
+  },
+  actions: {
+
+  },
+});
---
+++ mp3_player/src/views/Home.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+  #home
+    Bar
+    img.bg(src="../assets/black-and-white-sunset-clouds-field@3x.png")
+    .header Home
+    .main
+      h1 This is home page
+</template>
+
+<script>
+import Bar from '@/components/floatingBar.vue';
+
+export default {
+  components: {
+    Bar,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#home {
+  padding: 22px 24px;
+  text-align: center;
+  color: white;
+
+  .bg {
+    object-position: left;
+    object-fit: cover;
+    width: 100%;
+    height: 100vh;
+    position: absolute;
+    top: 0;
+    left: 0;
+    filter: blur(4px) brightness(80%);
+    z-index: -1;
+  }
+
+  .header {
+    font-size: 20px;
+  }
+}
+</style>
