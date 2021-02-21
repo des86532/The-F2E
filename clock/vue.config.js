b62c612d1808b8d41bf220646e0e2d802d572cd3
Tsu-Hao Yang <des75421@gmail.com>
2021-02-21 19:55:02 +0800

    demo

--- clock/vue.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  publicPath: '/The-F2E/clock/'
-}
--- multiplication-table/src/components/Footer.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="footer">
-    <p>Copyright © 2019 HexSchool. All rights reserved.</p>
-  </div>
-</template>
